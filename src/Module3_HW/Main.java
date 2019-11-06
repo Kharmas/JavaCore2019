d6fc8001244629bc107edfc16b27a01450f9d67f
Kharmas <kharmas14@gmail.com>
2019-11-06 17:54:40 +0200

    first commit

--- src/Module3_HW/Main.java
+++ src/Module3_HW/Main.java
@@ -12,12 +12,15 @@ public class Main {
         Course cour2 = new Course(123,"ex","Ed");
         Course cour3 = new Course(new Date(19,11,02),"testCourse1");
         Course cour4 = new Course(156,"ex1","Tim");
+
         Course courses [] = {cour1,cour2,cour3,cour4};
         Student stud2 = new Student("Green",courses);
         Student stud1 = new Student("Tom", "Lee",220);
+
         CollegeStuden cStud = new CollegeStuden("John","Wood",155);
         CollegeStuden cStud1 = new CollegeStuden("Mike","Green",120,"CSM",3,2652);
         CollegeStuden cSud2 = new CollegeStuden("Green",courses);
+
         SpecialStudent sStud = new SpecialStudent("Tim","Osborn",125);
         SpecialStudent sStud1 = new SpecialStudent("Osborn",courses);
         SpecialStudent sStud2 = new SpecialStudent("Tom","Lee",220,2523);
